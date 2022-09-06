11319021a0e1d8dec2e54376d81c6cbfc0542c9b
OAT <88110592+pongpairoch@users.noreply.github.com>
2022-09-07 00:06:34 +0700

    Update minMedMax.js

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,36 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
-}
+
+    let result = {min:"", mid:"", max:""}
+    let arr = [n1,n2,n3]
+    
+    let min = n1
+    let max = n1
+    let mid = n1
+  
+    for (let index = 0; index < arr.length; index++) {
+      if (arr[index] < min) {
+        min = arr[index]
+      }
+    }
+  
+    for (let index = 0; index < arr.length; index++) {
+      if (arr[index] > max) {
+        max = arr[index]
+      }
+    }
+  
+    for (let index = 0; index < arr.length; index++) {
+      if (arr[index] < max && arr[index] > min) {
+        mid = arr[index]
+      }
+    }
+  
+    result.min = min
+    result.mid = mid
+    result.max = max
+  
+    return result
+  }
 module.exports = minMedMax
